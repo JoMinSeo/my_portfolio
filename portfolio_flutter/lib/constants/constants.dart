7d1702a4d261a7da7fc65d3448b2678fde0dc747
JoMinSeo <49663640+JoMinSeo@users.noreply.github.com>
2021-02-03 13:37:11 +0900

    컴포넌트 분할

---
+++ portfolio_flutter/lib/constants/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+const kSchoolName = "KEY_SCHOOL_NAME";
+const kSchoolId = "KEY_SCHOOL_ID";
+const kLocate = "KEY_LOCATE";
+const kOfficeCode = "KEY_OFFICE_CODE";
+
+const kNaNumRegular = TextStyle(fontFamily: "NaNum");
+const kNaNumBold = TextStyle(fontFamily: "NaNum", fontWeight: FontWeight.w500);
+const kNaNumLight = TextStyle(fontFamily: "NaNum", fontWeight: FontWeight.w300);
+const kNaNumExtraBold =
+    TextStyle(fontFamily: "NaNum", fontWeight: FontWeight.w700);
+
+const kGrey = Color(0xFFEDF0F8);
+const kPrimary = Color(0xFF001E58);
+const kBlueGrey = Color(0xFF6E7C97);
+const kImproveGrey = Color(0xFFC5C8CF);
+const kLightGrey = Color(0xFFFBFBFB);
+const kBlue = Color(0xFF0D1440);
+const kRed = Color(0xFFBF1736);
